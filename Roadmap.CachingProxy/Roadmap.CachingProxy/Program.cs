e41a62e309e0cdd91e22ebdd1d8510b630403d78
Kıvanç Türker <kivanc.turker1997@gmail.com>
2025-01-23 02:04:00 +0300

    Forward request to orign without caching

--- Roadmap.CachingProxy/Roadmap.CachingProxy/Program.cs
+++ Roadmap.CachingProxy/Roadmap.CachingProxy/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualBasic.CompilerServices;
+﻿using System.Net;
+using System.Net.Sockets;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.VisualBasic.CompilerServices;
 
 namespace Roadmap.CachingProxy;
 
@@ -17,7 +20,7 @@ namespace Roadmap.CachingProxy;
 /*
  * TODO: (DONE) Initialize git and push the initial project.
  * TODO: (DONE) Program should accept the arguments as expected.
- * TODO: Start forwarding requests to the target url without caching.
+ * TODO: (DONE) Start forwarding requests to the target url without caching.
  * TODO: Start InMemory caching. No store.
  * TODO: Store the cached responses.
  * TODO: Remove the stored cached responses if --clear-cache flag provided.
@@ -25,9 +28,14 @@ namespace Roadmap.CachingProxy;
 
 class Program
 {
-    static void Main(string[] args)
+    private const int DEFAULT_PORT = 3030;
+    static async Task Main(string[] args)
     {
-        // Provide args and get (port, originUrl, isClearCache) 
+        if (!HttpListener.IsSupported)
+        {
+            Console.WriteLine("Http Listener Does not support in this OS.");
+        }
+        // Parsing arguments step
         int? port;
         string origin;
         bool isClearCache;
@@ -35,14 +43,65 @@ class Program
         try
         {
             (port, origin, isClearCache) = argument.ParseArguments(args);
-            Console.WriteLine($"port: {port}, origin: {origin}, isClearCache: {isClearCache}");
+            
+            // Forwarding step
+            // Listen on provided port
+            int validPort = port ?? DEFAULT_PORT;
+            HttpListener listener = StartServer(validPort);
+
+            HttpClient httpClient = new HttpClient();
+            Uri baseUri = new Uri(origin);
+            // Get Request
+            // https://learn.microsoft.com/en-us/dotnet/api/system.net.httplistener?view=net-9.0&redirectedfrom=MSDN
+            while (listener.IsListening)
+            {
+                // Get request
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                HttpListenerResponse response = context.Response;
+
+                HttpRequestMessage requestToForward =
+                    ConvertHttpListenerRequestToHttpRequestMessage(request, baseUri);
+
+                // Forward the requst to the origin
+                HttpResponseMessage responseFromOrigin = httpClient.Send(requestToForward);
+                byte[] buffer = await responseFromOrigin.Content.ReadAsByteArrayAsync();
+                response.ContentLength64 = buffer.Length;
+                Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+            }
         }
         catch (CustomException exception)
         {
             Console.Error.WriteLine(exception.Message);
             Environment.Exit(1);
         }
-        
-        
     }
+
+    private static HttpListener StartServer(int port)
+    {
+        string url = $"http://localhost:{port}/";
+        HttpListener listener = new HttpListener();
+        listener.Prefixes.Add(url);
+        listener.Start();
+        if (listener.IsListening)
+        {
+            Console.WriteLine($"Listening on {url}");
+            return listener;
+        }
+        throw new CustomException($"Problem While listening on {url}");
+    }
+
+    private static HttpRequestMessage ConvertHttpListenerRequestToHttpRequestMessage(HttpListenerRequest request, Uri baseUri)
+    {
+        Uri uri = new Uri(baseUri, request.Url.AbsolutePath);
+        HttpRequestMessage requestMessage = new HttpRequestMessage();
+        requestMessage.RequestUri = uri;
+        HttpMethod method = new HttpMethod(request.HttpMethod);
+        requestMessage.Method = method;
+
+        return requestMessage;
+    }
+        
 }
